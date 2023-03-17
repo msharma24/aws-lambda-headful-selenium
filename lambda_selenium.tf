0714e61b769614b0780ce8c6cec853e1f13421db
Mukesh Sharma <mukeshsharma24@gmail.com>
2023-03-17 13:21:48 +1300

    config update

--- lambda_selenium.tf
+++ lambda_selenium.tf
@@ -1,7 +1,7 @@
 module "selenium_lambda_function" {
   source = "terraform-aws-modules/lambda/aws"
 
-  function_name = "${var.environment}-selenium-schedule-lambda"
+  function_name = "${var.environment}-selenium-headful-lambda"
   description   = "Selenium lambda function"
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.8"
@@ -10,16 +10,16 @@ module "selenium_lambda_function" {
   memory_size = "128"
   timeout     = "60"
 
-  source_path           = "./python/lambda_function.py"
-  attach_network_policy = true
+  source_path            = "./python/lambda_function.py"
+  local_existing_package = module.package_dir.local_filename
+  create_package         = false
+  attach_network_policy  = true
 
 
   vpc_subnet_ids         = [aws_subnet.nat_gw_subnet.id]
   vpc_security_group_ids = [module.selenium_lambda_security_group.security_group_id]
 
-  environment_variables = {
-    TEST_ENV_VAR = "Hello Test Env var"
-  }
+
 
   tags = {
     Name = "${var.environment}-selenium-Lambda"
--- provider.tf
+++ provider.tf
@@ -1,3 +1,6 @@
 provider "aws" {
   region = var.aws_region
+
+  skip_get_ec2_platforms  = true
+  skip_metadata_api_check = true
 }
